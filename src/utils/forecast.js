08e2c324f8ccb1d4109b45c4497036e54318404e
Sai Sriram <saisriram.sharma@gmail.com>
2020-09-01 13:44:17 +0530

    Add about text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('postman-request')
 
+
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=6296e87068b6e1d7103491287198a306&query='+latitude+',' +longitude+'&units=m'
 
@@ -9,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0]+". Currently it is " + response.body.current.temperature +" "+"degrees out. It feels like "+ response.body.current.feelslike+" "+"degrees out")
+
+            callback(undefined, response.body.current.weather_descriptions[0]+". Currently it is " + response.body.current.temperature +" "+"degrees out. It feels like "+ response.body.current.feelslike+" "+"degrees out. The humidity is "+response.body.current.humidity+"%.")
         }
     })
 }
